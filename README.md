cf9c2566a54e700469008b51c28c7c9919b6c923
gzock <gzock0308@hotmail.co.jp>
2020-12-27 16:56:10 +0900

    add export cli tool
    fix https://github.com/gzock/tocaro-exporter/issues/1

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # 概要
 
-2020年9月1日以降の大規模アップデートが適用された[Tocaro](https://tocaro.im/)からメッセージの一覧をブッコ抜くスクリプト。  
-別のチャットツールなどに移行する際に便利かも？ jsonで出力するので、あとはよしなに。
+2020年9月1日以降の大規模アップデートが適用された[Tocaro](https://tocaro.im/)からメッセージの一覧をブッコ抜くスクリプト。
+
+大規模アップデート後は、グループやメッセージ取得にREST API使うようになったので、それを利用させてもらっている。  
+ブラウザで人間が操作している内容を模倣している感じ。
+
+別のチャットツールなどに移行する際に便利かも？ jsonで出力するので、あとはよしなに。ただし全て自己責任で。
 
 # 動作環境
 
@@ -11,6 +15,12 @@
 * Python 3.6.9
 
 # 使い方
+
+## 下準備
+
+```
+$ pip3 install -r requirements.txt
+```
 
 ## コンフィグの準備
 
@@ -24,21 +34,70 @@ group_type|show|メッセージを取得する対象のグループタイプ。s
 excludes|アラート|指定した文字列に部分一致する名前のグループはメッセージ取得しない。
 interval|0.3|連続的にメッセージ取得する際の一定の停止間隔(秒)<br>0に指定すると停止なしの最速処理(非推奨)
 debug|false|デバッグログを出力するかどうか
-with_groups|true|グループ一覧を出力するかどうか
 path|./outputs|出力先パス
 
 ## 実行
 
-とっても簡単。
+所属している全グループから全メッセージを出力。※実行完了までかなり時間がかかる
 
 ```
-$ ./main.py
+$ ./tocaro_exporter.py --all
+```
+
+指定された単一グループの全メッセージのみを出力。
+
+```
+$ ./tocaro_exporter.py --group-id [group-id]
+```
+
+指定された文字列をグループ名に含むグループの全メッセージを出力。
+
+```
+$ ./tocaro_exporter.py --includes [string]
+```
+
+所属している全グループの一覧のみを出力。※メッセージの出力は行わない
+
+```
+$ ./tocaro_exporter.py --group-only
+```
+
+### コンフィグファイルがカレントディレクトリ内に存在しない場合
+
+デフォルトではカレントディレクトリ内の`config.ini`をコンフィグファイルとして読み込む。  
+異なるパスやファイル名であった場合は、`--config`によって変更可能。
+
+### オプション一覧
+
+```
+./tocaro_exporter.py -h
+usage: tocaro_exporter.py [-h] [-c CONFIG] [-a] [-g GROUP_ID] [-i INCLUDES]
+                          [--group-only]
+
+Tocaro Exporter
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -c CONFIG, --config CONFIG
+                        specify config file path.
+  -a, --all             export all messages from all groups.
+  -g GROUP_ID, --group-id GROUP_ID
+                        export messages from specified group.
+  -i INCLUDES, --includes INCLUDES
+                        export message from groups including the specified
+                        string.
+  --group-only          export for group list json only.
 ```
 
 # 注意
 
 * TocaroのAPIの都合上、メッセージを30件毎に取得する必要がある。そのためメッセージ量が多いと全てを取得しきるのに相当な時間がかかる。気長に待つべし。
+  * 参考程度に・・・以下の条件の全メッセージ出力に1時間程度かかった実績あり
+    * グループ/トーク合計数: 110
+    * メッセージ総数: 20万弱
+    * interval: 0.4
 * グループが多いと、その分時間がかかる。不要なグループは非対象になるよう`excludes`には何かしらの文字列は入れておいたほうが良い。
+  * `--includes`を併用した場合、excludesによる除外が行われたリストに対して、includesによる処理がかかる
 
 # 参考
 
--- tocaro_exporter.py
+++ tocaro_exporter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import os
+from argparse import ArgumentParser
 from configparser import ConfigParser
 from logging import getLogger, basicConfig, INFO, DEBUG
 import json
@@ -8,63 +9,122 @@ from distutils.util import strtobool
 
 from tocaro_session import TocaroSession
 
-CONFIG_INI = "./config.ini"
-
-def save_json(content, output_path):
-  with open(output_path, 'w', encoding='utf-8') as f:
-    json.dump(content, f, indent=4, ensure_ascii=False)
-
-def main():
+class TocaroExporter():
   logger = getLogger(__name__)
+  group_type = "show"
+  interval = 0.3
 
+  def __init__(self, email="", password=""):
+    self.tocaro = TocaroSession()
+    if email and password:
+      self.signin(email, password)
+
+  def signin(self, email, password):
+    self.tocaro.signin(email, password)
+
+  def get_groups(self):
+    if self.group_type not in ["show", "hide"]:
+      self.logger.error("group_type invalid. valid value: show, hide")
+      raise Exception
+
+    return self.tocaro.get_groups(self.group_type)
+
+  def export_groups(self, output_path):
+    groups = self.get_groups()
+
+    __path = "%s/groups.json" % output_path
+    self.logger.info("groups saving... output path: " + __path)
+    self.__save_json(groups, __path)
+
+  def gather_group_ids(self, groups, includes="", excludes=""):
+    ret = []
+    for group in groups:
+      if group["type"] not in ["group", "talk"]:
+        self.logger.debug("group_id: %s, group type isn't group or talk. processing will be skip." % group["code"])
+        continue
+
+      if excludes and excludes in group["name"]:
+        self.logger.debug("group_id: %s, contain excudes string in group name. processing will be skip." % group["code"])
+        continue
+
+      if includes and includes not in group["name"]:
+        self.logger.debug("group_id: %s, not include string in group name. processing will be skip." % group["code"])
+        continue
+
+      ret.append(group["code"])
+    return ret
+    
+  def export_messages(self, output_path, group_id="", includes="", excludes=""):
+    group_ids = []
+
+    if group_id:
+      group_ids.append(group_id)
+
+    else:
+      groups = self.get_groups()
+      group_ids.extend(
+          self.gather_group_ids(groups, includes, excludes)
+      )
+
+    self.logger.info("gatherd group id list: " + str(group_ids))
+
+    for group_id in group_ids:
+      self.logger.info("get message from group_id: " + group_id)
+      messages = self.tocaro.get_all_messages(
+          group_id=group_id,
+          interval=float(self.interval)
+      )
+      __path = "%s/%s.json" % (output_path, group_id)
+      self.logger.info("messages saving... output path: " + __path)
+      self.__save_json(messages, __path)
+
+  def __save_json(self, content, output_path):
+    with open(output_path, 'w', encoding='utf-8') as f:
+      json.dump(content, f, indent=4, ensure_ascii=False)
+
+
+def InvalidArgsError(Exception):
+  pass
+  
+def main(args):
+  logger = getLogger(__name__)
   logger.info("tocaro exporter execution start.")
+
+  if 2 > sum(list(map(lambda x: bool(vars(args)[x]), vars(args)))):
+    raise InvalidArgsError
+
   logger.info("reading config...")
   config = ConfigParser()
-  if not os.path.exists(CONFIG_INI):
-    logger.error("config.ini not exists.")
+  if not os.path.exists(args.config):
+    logger.error("specified config file not exists.")
     exit(1)
-  config.read(CONFIG_INI, encoding='utf-8')
+  config.read(args.config, encoding='utf-8')
 
   if strtobool(config["common"]["debug"]):
     getLogger().setLevel(DEBUG)
     logger.debug("enable debug logging.")
 
   logger.info("connect to tocaro.")
-  tocaro = TocaroSession()
-  tocaro.signin(
+  exporter = TocaroExporter(
       email=config["account"]["email"], 
       password=config["account"]["password"]
   )
 
-  group_type = config["common"]["group_type"]
-  if group_type not in ["show", "hide"]:
-    logger.error("group_type invalid. valid value: show, hide")
-    raise Exception
+  exporter.group_type = config["common"]["group_type"]
+  exporter.interval = config["common"]["interval"]
 
-  groups = tocaro.get_groups(group_type)
-  if strtobool(config["output"]["with_groups"]):
-    output_path = "%s/groups.json" % config["output"]["path"]
-    save_json(groups, output_path)
+  if args.group_only:
+    exporter.export_groups(config["output"]["path"])
 
-  for group in groups:
-    if group["type"] not in ["group", "talk"]:
-      logger.debug("group_id: %s, group type isn't group or talk. processing will be skip." % group["code"])
-      continue
-
-    if config["common"]["excludes"] in group["name"]:
-      logger.debug("group_id: %s, contain excudes string in group name. processing will be skip." % group["code"])
-      continue
-
-    logger.info("get message from group_id: " + group["code"])
-    messages = tocaro.get_all_messages(
-        group_id=group["code"],
-        interval=float(config["common"]["interval"])
+  else:
+    exporter.export_messages(
+        output_path=config["output"]["path"],
+        group_id=args.group_id,
+        includes=args.includes,
+        excludes=config["common"]["excludes"]
     )
+  logger.info("processing sucessfully.")
 
-    output_path = "%s/%s.json" % (config["output"]["path"], group["code"])
-    logger.info("message saving... output path: " + output_path)
-
-    save_json(messages, output_path)
 
 if __name__ == "__main__":
   basicConfig(
@@ -73,4 +133,13 @@ if __name__ == "__main__":
   )
 
   getLogger().setLevel(INFO)
-  main()
+
+  parser = ArgumentParser(description="Tocaro Exporter")
+  parser.add_argument("-c", "--config", default="./config.ini", help="specify config file path.")
+  parser.add_argument("-a", "--all", action="store_true", help="export all messages from all groups.")
+  parser.add_argument("-g", "--group-id", help="export messages from specified group.")
+  parser.add_argument("-i", "--includes", help="export message from groups including the specified string.")
+  parser.add_argument("--group-only", action="store_true", help="export for group list json only.")
+  args = parser.parse_args()  
+
+  main(args)
